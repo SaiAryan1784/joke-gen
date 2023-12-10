a0d94105d244c31ab2c8b28d20bb0a9ce8fd7a4d
Sai Aryan Goswami <saiaryan.goswami1784@gmail.com>
2023-12-10 17:30:53 +0530

    Used fetch function in js to request a joke from url given and display it on html using dom

---
+++ apijoke.js
@@ -0,0 +1,16 @@
+const jokeContainer = document.getElementById("joke");
+const btn = document.getElementById("btn");
+const url = "https://v2.jokeapi.dev/joke/Programming?type=single"
+
+let getJoke = () => {
+    jokeContainer.classList.remove("fade")
+    fetch(url)
+    .then(data => data.json())
+    .then(item => {
+        jokeContainer.classList.add("fade")
+        jokeContainer.textContent = `${item.joke}`
+    })
+}
+
+btn.addEventListener("click",getJoke);
+getJoke();
